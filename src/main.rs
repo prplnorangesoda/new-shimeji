f96936edbf47786f6cced6376bf81d1678f4ae42
Lucy Faria <rafploooz@gmail.com>
2024-11-21 13:07:36 -0500

    fix memory leak and unsafe blocks with Rc

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,46 @@
 #![deny(unused_must_use)]
 use anyhow::Context as ContextTrait;
 use softbuffer::{Context, Surface};
+use std::hash::Hash;
 use std::num::NonZeroU32;
+use std::rc::Rc;
 use tiny_skia::{Color, FillRule, Paint, PathBuilder, Pixmap, Stroke, Transform};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::window::{Fullscreen, Window, WindowId};
+use winit::window::{Fullscreen, Window, WindowId, WindowLevel};
 #[derive(Default)]
-struct App<'a> {
-    window: Option<Window>,
-    context: Option<Context<&'a Window>>,
-    surface: Option<Surface<&'a Window, &'a Window>>,
+struct App {
+    window: Option<Rc<Window>>,
+    context: Option<Context<Rc<Window>>>,
+    surface: Option<Surface<Rc<Window>, Rc<Window>>>,
 }
 
-impl<'a> ApplicationHandler for App<'a> {
+impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(
+        self.window = Some(Rc::new(
             event_loop
                 .create_window(
                     Window::default_attributes()
-                        .with_window_level(winit::window::WindowLevel::AlwaysOnTop)
+                        .with_visible(false)
+                        .with_window_level(WindowLevel::AlwaysOnTop)
                         .with_decorations(false)
+                        .with_transparent(true)
                         .with_title("HEYYYYYYYYY"),
                 )
                 .unwrap(),
-        );
-        // one of these two unsafe blocks causes a double free and a SIGSEGV
-        // solution: make a new wrapper struct ?
-        self.context = unsafe {
-            // we know the window will be valid for as long as the struct, since we just created it!
-            // lifetime trickery :(
-            std::mem::transmute(Some(Context::new(self.window.as_ref().unwrap()).unwrap()))
-        };
+        ));
 
-        self.surface = unsafe {
-            std::mem::transmute(Some(
-                Surface::new(
-                    self.context.as_ref().unwrap(),
-                    self.window.as_ref().unwrap(),
-                )
-                .unwrap(),
-            ))
-        }
+        self.context = Some(Context::new(self.window.as_ref().unwrap().clone()).unwrap());
+
+        self.surface = Some(
+            Surface::new(
+                self.context.as_ref().unwrap(),
+                self.window.as_ref().unwrap().clone(),
+            )
+            .unwrap(),
+        );
+        self.window.as_ref().unwrap().request_redraw();
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
@@ -51,7 +49,7 @@ impl<'a> ApplicationHandler for App<'a> {
                 println!("The close button was pressed; stopping");
                 event_loop.exit();
             }
-            WindowEvent::RedrawRequested if matches!(self.window.as_ref(), Some(..)) => 'redraw: {
+            WindowEvent::RedrawRequested if matches!(self.window.as_ref(), Some(_)) => 'redraw: {
                 let window = self.window.as_mut().unwrap();
                 let surface = self.surface.as_mut().unwrap();
                 let context = self.context.as_ref().unwrap();
@@ -81,7 +79,7 @@ impl<'a> ApplicationHandler for App<'a> {
                     .unwrap();
 
                 let mut pixmap = Pixmap::new(width, height).unwrap();
-                pixmap.fill(Color::WHITE);
+                pixmap.fill(Color::from_rgba8(0, 0, 0, 255));
                 let path = PathBuilder::from_circle(
                     (width / 2) as f32,
                     (height / 2) as f32,
@@ -89,7 +87,7 @@ impl<'a> ApplicationHandler for App<'a> {
                 )
                 .unwrap();
                 let mut paint = Paint::default();
-                paint.set_color_rgba8(255, 128, 128, 128);
+                paint.set_color_rgba8(255, 128, 128, 255);
                 pixmap.fill_path(
                     &path,
                     &paint,
@@ -111,13 +109,14 @@ impl<'a> ApplicationHandler for App<'a> {
 
                 buffer.present().unwrap();
                 // Draw.
+                window.set_visible(true);
 
                 // Queue a RedrawRequested event.
                 //
                 // You only need to call this if you've determined that you need to redraw in
                 // applications which do not always need to. Applications that redraw continuously
                 // can render here instead.
-                self.window.as_ref().unwrap().request_redraw();
+                // window.request_redraw();
             }
             _ => (),
         }
@@ -127,14 +126,9 @@ impl<'a> ApplicationHandler for App<'a> {
 fn main() -> anyhow::Result<()> {
     let event_loop = EventLoop::new().unwrap();
 
-    // This Leaks Memory
     let mut app = App::default();
     event_loop
         .run_app(&mut app)
         .context("Event loop broke running app")?;
-
-    // If we do not forcibly exit, drop(app) gets called and SIGSEGV happens
-    // App is memory unsafe - we must rely on the OS to free it when we are done
-    // Could refactor to fix app's memory unsafety but lowkey not feeling it right now
-    std::process::exit(0);
+    Ok(())
 }
