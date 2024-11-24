2532e41bca5022345d0b851291a91323a29a3a63
Lucy <rafploooz@gmail.com>
2024-11-24 17:27:51 -0500

    Large refactor - seems like I'm going to have to implement a pixel renderer

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,143 @@
 #![deny(unused_must_use)]
-use anyhow::Context as ContextTrait;
-use winit::event_loop::EventLoop;
+use anyhow::Context as _;
+use std::{num::NonZeroU32, rc::Rc};
+use tao::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
 
-mod app;
+#[derive(Clone, Copy, PartialEq, Eq)]
+pub struct RGBA {
+    red: u8,
+    green: u8,
+    blue: u8,
+    alpha: u8,
+}
 
-use app::App;
+impl RGBA {
+    pub fn new(red: u8, green: u8, blue: u8, alpha: u8) -> Self {
+        RGBA {
+            red,
+            green,
+            blue,
+            alpha,
+        }
+    }
 
+    pub fn from_tuple<T>(tuple: T) -> Self
+    where
+        T: Into<(u8, u8, u8, u8)>,
+    {
+        let rgba = tuple.into();
+        RGBA {
+            red: rgba.0,
+            green: rgba.1,
+            blue: rgba.2,
+            alpha: rgba.3,
+        }
+    }
+
+    /// --------
+    ///
+    /// Pixel format (`u32`):
+    ///
+    /// 00000000RRRRRRRRGGGGGGGGBBBBBBBB
+    ///
+    /// 0: Bit is 0
+    /// R: Red channel
+    /// G: Green channel
+    /// B: Blue channel
+    pub fn to_softbuf_u32(&self) -> u32 {
+        if self.alpha == 0 {
+            return 0;
+        }
+        // println!("self.alpha as f32: {}", self.alpha as f32);
+        // let alpha = self.alpha as f32 / 255.0;
+        // let red = (self.red as f32 * alpha).floor() as u32;
+        // let blue = (self.blue as f32 * alpha).floor() as u32;
+        // let green = (self.green as f32 * alpha).floor() as u32;
+        // println!("red: {}", red);
+        // println!("green: {}", green);
+        // println!("blue: {}", blue);
+        // println!("alpha: {}", alpha);
+
+        // let ret = (red << 16) | (blue << 8) | green;
+
+        // println!("ret: {:#034b}", ret);
+        // ret
+
+        (self.alpha as u32) << 24
+            | (self.red as u32) << 16
+            | (self.blue as u32) << 8
+            | self.green as u32
+    }
+}
 fn main() -> anyhow::Result<()> {
-    let event_loop = EventLoop::new().unwrap();
+    let event_loop = EventLoop::new();
 
-    let mut app = App::default();
-    event_loop
-        .run_app(&mut app)
-        .context("Event loop broke running app")?;
-    Ok(())
+    let window = WindowBuilder::new()
+        .with_decorations(false)
+        .with_transparent(true)
+        .with_always_on_top(true)
+        .build(&event_loop)
+        .context("Building initial window failed")?;
+
+    let (window, _context, mut surface) = {
+        let window = Rc::new(window);
+        let context = softbuffer::Context::new(window.clone()).unwrap();
+        let surface = softbuffer::Surface::new(&context, window.clone()).unwrap();
+        (window, context, surface)
+    };
+    window.set_title("Awesome window!");
+    // if cfg!(target_os = "linux") {
+    //     native_dialog::MessageDialog::new()
+    //         .set_title("info")
+    //         .set_type(native_dialog::MessageType::Warning)
+    //         .set_text("If you're on Wayland, you must set this window to show on top specifically.")
+    //         .show_alert()
+    //         .ok();
+    // }
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+        // println!("{event:?}");
+
+        match event {
+            Event::LoopDestroyed => {
+                println!("Bye!")
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                *control_flow = ControlFlow::Exit;
+            }
+
+            Event::RedrawRequested(_) => {
+                let (width, height) = {
+                    let size = window.inner_size();
+                    (size.width, size.height)
+                };
+                surface
+                    .resize(
+                        NonZeroU32::new(width).unwrap(),
+                        NonZeroU32::new(height).unwrap(),
+                    )
+                    .unwrap();
+
+                let mut buffer = surface.buffer_mut().unwrap();
+                println!("Buffer length: {}", buffer.len());
+                println!(
+                    "First four buffer bytes: {:b} {:b} {:b} {:b}",
+                    buffer[0], buffer[1], buffer[2], buffer[3]
+                );
+                let color_u32 = RGBA::new(0, 0, 0, 128).to_softbuf_u32();
+                buffer.fill(color_u32);
+                buffer.present().unwrap();
+            }
+
+            _ => (),
+        }
+    });
 }
